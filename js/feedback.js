1d19d6abd0cd24661cd856794371eaec3a0a5431
Владимир Санников <cs7@yandex.ru>
2019-10-25 15:33:09 +0300

    Исправления по ТЗ
    ТЗ 3.12 - карточки на главной так же, как в каталоге
    Добавил оверлей для модалки обратной связи

--- js/feedback.js
+++ js/feedback.js
@@ -1,14 +1,17 @@
 /* обработка обратной связи */
 var feedback_button = document.querySelector(".feedback-button");
 var modal_feedback = document.querySelector(".modal-feedback");
-var feedback_close = document.querySelector(".modal-feedback .modal-close")
+var feedback_close = document.querySelector(".modal-feedback .modal-close");
+var overlay = document.querySelector(".overlay");
 
 feedback_button.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	show(modal_feedback);
+	show(overlay)
 });
 
 feedback_close.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	hide(modal_feedback);
+	hide(overlay);
 });
